253ec54d4bf84e8189cc6cbc673657628f67605b
0925648 <svenkalkman1999@gmail.com>
2018-06-12 17:24:39 +0200

    Added buttons

--- Serious-Gaming-Local/Serious-Gaming-Local/Form1.cs
+++ Serious-Gaming-Local/Serious-Gaming-Local/Form1.cs
@@ -16,5 +16,55 @@ namespace Serious_Gaming_Local
     {
       InitializeComponent();
     }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void button1_MouseEnter(object sender, EventArgs e)
+    {
+      button1.BackColor = Color.Blue;
+    }
+
+    private void button1_MouseLeave(object sender, EventArgs e)
+    {
+      button1.BackColor = Color.White;
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void button2_MouseEnter(object sender, EventArgs e)
+    {
+      button2.BackColor = Color.Blue;
+    }
+
+    private void button2_MouseLeave(object sender, EventArgs e)
+    {
+      button2.BackColor = Color.White;
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+      Application.Exit();
+    }
+
+    private void button3_MouseEnter(object sender, EventArgs e)
+    {
+      button3.BackColor = Color.Blue;
+    }
+
+    private void button3_MouseLeave(object sender, EventArgs e)
+    {
+      button3.BackColor = Color.White;
+    }
   }
 }
