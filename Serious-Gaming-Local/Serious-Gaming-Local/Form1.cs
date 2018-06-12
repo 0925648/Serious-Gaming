7a3695bd030a2fd2fc35b7862ac84dc690bdf15b
0925648 <svenkalkman1999@gmail.com>
2018-06-12 16:06:54 +0200

    initial commit

---
+++ Serious-Gaming-Local/Serious-Gaming-Local/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Serious_Gaming_Local
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+  }
+}
